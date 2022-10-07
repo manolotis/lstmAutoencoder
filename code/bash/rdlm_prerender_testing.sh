92fd9b1c6840e4df2d5be08fb3979ddba5d11259
Manu <manuel.munoz.sanchez@gmail.com>
2022-10-07 11:49:22 +0200

    Prerender scripts with higher number of jobs

--- code/bash/rdlm_prerender_testing.sh
+++ code/bash/rdlm_prerender_testing.sh
@@ -3,4 +3,4 @@ python /home/manolotis/sandbox/robustness_benchmark/lstmAutoencoder/code/prerend
 --data-path "/media/disk1/sandbox/waymoMotion/data/reduced/tf_example/testing/" \
 --output-path "/home/manolotis/sandbox/robustness_benchmark/lstmAutoencoder/data/prerendered/testing" \
 --config "/home/manolotis/sandbox/robustness_benchmark/lstmAutoencoder/code/configs/prerender.yaml" \
---n-jobs 8
+--n-jobs 48
--- code/bash/rdlm_prerender_val.sh
+++ code/bash/rdlm_prerender_val.sh
@@ -3,4 +3,4 @@ python /home/manolotis/sandbox/robustness_benchmark/lstmAutoencoder/code/prerend
 --data-path "/media/disk1/sandbox/waymoMotion/data/reduced/tf_example/validation/" \
 --output-path "/home/manolotis/sandbox/robustness_benchmark/lstmAutoencoder/data/prerendered/validation" \
 --config "/home/manolotis/sandbox/robustness_benchmark/lstmAutoencoder/code/configs/prerender.yaml" \
---n-jobs 8
+--n-jobs 48
