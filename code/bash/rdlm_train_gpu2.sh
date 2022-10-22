d2a59da2fd63b3201b860733a9b4e3676591f009
Manu <manuel.munoz.sanchez@gmail.com>
2022-10-22 11:37:13 +0200

    fix paths

--- code/bash/rdlm_train_gpu2.sh
+++ code/bash/rdlm_train_gpu2.sh
@@ -1,17 +1,17 @@
 CUDA_VISIBLE_DEVICES=2 \
 python /home/manolotis/sandbox/robustness_benchmark/lstmAutoencoder/code/train.py \
-  --train-data-path "/home/manolotis/datasets/waymo/motion v1.0/prerender/multipathPP/training" \
-  --val-data-path "/home/manolotis/datasets/waymo/motion v1.0/prerender/multipathPP/validation" \
+  --train-data-path "/home/manolotis/datasets/waymo/motion v1.0/prerender/lstmAutoencoder/training" \
+  --val-data-path "/home/manolotis/datasets/waymo/motion v1.0/prerender/lstmAutoencoder/validation" \
   --config "/home/manolotis/sandbox/robustness_benchmark/lstmAutoencoder/code/configs/train_1x128.yaml" \
 
 CUDA_VISIBLE_DEVICES=2 \
 python /home/manolotis/sandbox/robustness_benchmark/lstmAutoencoder/code/train.py \
-  --train-data-path "/home/manolotis/datasets/waymo/motion v1.0/prerender/multipathPP/training" \
-  --val-data-path "/home/manolotis/datasets/waymo/motion v1.0/prerender/multipathPP/validation" \
+  --train-data-path "/home/manolotis/datasets/waymo/motion v1.0/prerender/lstmAutoencoder/training" \
+  --val-data-path "/home/manolotis/datasets/waymo/motion v1.0/prerender/lstmAutoencoder/validation" \
   --config "/home/manolotis/sandbox/robustness_benchmark/lstmAutoencoder/code/configs/train_2x64.yaml" \
 
 CUDA_VISIBLE_DEVICES=2 \
 python /home/manolotis/sandbox/robustness_benchmark/lstmAutoencoder/code/train.py \
-  --train-data-path "/home/manolotis/datasets/waymo/motion v1.0/prerender/multipathPP/training" \
-  --val-data-path "/home/manolotis/datasets/waymo/motion v1.0/prerender/multipathPP/validation" \
+  --train-data-path "/home/manolotis/datasets/waymo/motion v1.0/prerender/lstmAutoencoder/training" \
+  --val-data-path "/home/manolotis/datasets/waymo/motion v1.0/prerender/lstmAutoencoder/validation" \
   --config "/home/manolotis/sandbox/robustness_benchmark/lstmAutoencoder/code/configs/train_3x512.yaml" \
