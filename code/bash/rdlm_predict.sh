06f46f510376839c63457fda51ffb1e5c8428073
manolotis <manuel.munoz.sanchez@gmail.com>
2023-11-02 17:42:34 +0100

    fixed rdlm predict script

--- code/bash/rdlm_predict.sh
+++ code/bash/rdlm_predict.sh
@@ -1,11 +1,11 @@
-TEST_DATA_PATH="/home/manolotis/datasets/waymo/motion v1.0/prerender/lstmEncoderDecoder/testing"
+TEST_DATA_PATH="/home/manolotis/datasets/waymo/motion v1.0/prerender/lstmAutoencoder/testing"
 OUT_PATH="/home/manolotis/sandbox/scenario_based_evaluation/lstmEncoderDecoder/predictions/"
 BATCH_SIZE=128
 N_JOBS=48
 BASE_CONFIG="/home/manolotis/sandbox/scenario_based_evaluation/lstmEncoderDecoder/code/configs/predict.yaml"
 BASE_SCRIPT="/home/manolotis/sandbox/scenario_based_evaluation/lstmEncoderDecoder/code/predict.py"
 
-python $BASE_SCRIPT --config $BASE_CONFIG --test-data-path $TEST_DATA_PATH --batch-size $BATCH_SIZE --n-jobs $N_JOBS --out-path $OUT_PATH \
+python $BASE_SCRIPT --config $BASE_CONFIG --test-data-path "$TEST_DATA_PATH" --batch-size $BATCH_SIZE --n-jobs $N_JOBS --out-path $OUT_PATH \
   --model-name lstm_3x128__0303335 --num-layers 3 --hidden-size 128
 
 
